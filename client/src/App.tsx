73d048ba844cdc06171908a688d814e1e6749309
이태훈 <rhymint@gmail.com>
2024-01-05 09:05:18 +0900

    design: header 폰트 크기 및 버튼 크기 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,18 +6,19 @@ import Apply from './pages/Apply';
 
 function App() {
   return (
+    <div className="bg-slate-100">
+      <div className="flex flex-row justify-center min-h-screen min-w-[320px]">
+        <div className="grow z-10 bg-slate-100" />
+        <div className="max-w-3xl w-screen min-w-[320px] bg-white">
+          <Header />
 
-    <div className="flex flex-row justify-center min-h-screen min-w-[320px]">
-      <div className="grow z-10 bg-slate-100" />
-      <div className="max-w-3xl w-screen min-w-[320px]">
-        <Header />
-
-        <Routes>
-          <Route path='/' Component={Main} />
-          <Route path='/apply' Component={Apply} />
-        </Routes>
+          <Routes>
+            <Route path='/' Component={Main} />
+            <Route path='/apply' Component={Apply} />
+          </Routes>
+        </div>
+        <div className="grow z-10 bg-slate-100" />
       </div>
-      <div className="grow z-10 bg-slate-100" />
     </div>
   )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -8,18 +8,18 @@ const Header = () => {
   const goHome = () => { navigate('/') }
 
   return (<div className='fixed top-0 px-8 w-full min-w-[320px] max-w-3xl flex flex-row gap-4 h-16 items-center bg-white drop-shadow-lg z-50'>
-    <div className='grow pt-3 pb-0.5 flex items-center justify-start'>
+    <div className='grow pt-3 pb-1 flex items-center justify-start'>
       <motion.div className="top-0" onClick={goHome}
         whileTap={{
           scale: 0.95,
           transition: { duration: 0.1 }
         }}>
-        <div className="text-left text-4xl font-BMDOHYUN text-stone-800 truncat">
+        <div className="text-left text-2xl font-BMDOHYUN text-stone-800 truncat">
           {STRING.headerLogo}
         </div>
       </motion.div>
     </div>
-    <div className='basis-1/4 h-full py-3'>
+    <div className='basis-1/4 h-full py-3.5'>
       <motion.div className='bg-pink-400 h-full rounded-lg flex items-center justify-center' onClick={goApply}
         whileTap={{
           scale: 0.95,
