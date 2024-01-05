1456319789feafc2338cae49acd329b34418fb8b
이태훈 <rhymint@gmail.com>
2024-01-05 20:48:37 +0900

    design: privacyModal 작은 해상도에도 대응할 수 있도록 여백 및 폰트 크기 변경

--- client/src/components/PrivacyModal.tsx
+++ client/src/components/PrivacyModal.tsx
@@ -9,45 +9,47 @@ const PrivacyModal = ({ handleOpenedChange }: PrivacyModalProp) => {
 
   return (
     <div className="flex fixed inset-0 z-20 w-screen h-screen bg-stone-800 bg-opacity-50 items-center justify-center"
-      // onClick={handleOpenedChange}
+    // onClick={handleOpenedChange}
     >
-      <div className="z-30 h-3/4 w-5/6 rounded-3xl bg-white max-w-2xl max-h-[720px]">
-        <div className="h-full flex flex-col py-16 px-8 items-center">
-          <div className="font-BMDOHYUN text-stone-800 text-center">
+      <div className="z-30 h-5/6 w-5/6 rounded-3xl bg-white max-w-2xl max-h-[720px]">
+        <div className="h-full flex flex-col p-8 items-center">
+          <div className="grow flex font-BMDOHYUN text-stone-800 text-l text-center items-center">
             {STRING.privacyModalTitle}
           </div>
-          <div className='grow flex flex-col gap-8 py-8 max-w-sm'>
-            <div className="text-stone-800">
+          <div className='shrink flex flex-col gap-8 py-4 max-w-sm justify-center items-center'>
+            <div className="text-stone-800 text-base">
               {STRING.privacyModalStart}
             </div>
-            <div className="flex flex-col gap-2">
-              <div className="text-stone-800">
+            <div className="flex flex-col gap-0.5">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP1}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP2}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP3}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP4}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP5}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP6}
               </div>
-              <div className="text-stone-800">
+              <div className="text-stone-800 text-base">
                 {STRING.privacyModalP7}
               </div>
             </div>
-            <div className="text-stone-800">
+            <div className="text-stone-800 text-base">
               {STRING.privacyModalEnd}
             </div>
           </div>
-          <AmsrButton title={'닫기'} disabled={false} onClick={handleOpenedChange} />
+          <div className="grow flex items-center">
+            <AmsrButton title={'닫기'} disabled={false} onClick={handleOpenedChange} />
+          </div>
         </div>
       </div>
     </div>
