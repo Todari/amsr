f63aaa35616a4ed44469b9c7b40bc434588bba4d
이태훈 <rhymint@gmail.com>
2024-01-17 19:57:53 +0900

    design: header 디자인 변경

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import STRING from "../constants/String";
 import { useEffect, useState } from "react";
 import AmsrButton from "../components/AmsrButton";
+import {ReactComponent as AMSR_LOGO} from "../asset/amsr-logo.svg"
 
 type HeaderProp = {
   visible: boolean
@@ -22,21 +23,22 @@ const Header = ({ visible }: HeaderProp) => {
     }
   }, [visible])
 
-  return (<motion.div className='fixed top-0 px-8 w-full flex flex-row gap-4 h-16 items-center bg-white drop-shadow-lg z-10'
+  return (<motion.div className='fixed top-0 px-8 w-full flex flex-row gap-4 h-16 items-center justify-center bg-white drop-shadow-lg z-10'
     animate={{ y }}
     transition={{ duration: 0.3 }}>
-    <div className='grow pt-3 pb-1 flex items-center justify-start'>
+    <div className='grow flex items-center justify-start max-w-3xl'>
       <motion.div className="top-0" onClick={goHome}
         whileTap={{
           scale: 0.95,
           transition: { duration: 0.1 }
         }}>
-        <div className="text-left text-2xl text-stone-800 truncat">
+        {/* <div className="text-left text-2xl text-stone-800 truncat">
           {STRING.headerLogo}
-        </div>
+        </div> */}
+        <AMSR_LOGO className="aspect-square h-24"/>
       </motion.div>
     </div>
-    <div className='basis-1/4 h-full py-3.5 flex justify-center'>
+    <div className='shrink h-full py-3.5 flex justify-center'>
       <AmsrButton title={STRING.headerApplyButton} variant="secondary" onClick={goApply} />
     </div>
   </motion.div >)
